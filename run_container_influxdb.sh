1d026c90e44d2e2aed381a12229a912f34ec1d19
Raphael Wirtz <raphael9w@gmail.com>
2023-10-16 11:06:45 +0200

    add: influxdb and python example

--- run_container_influxdb.sh
+++ run_container_influxdb.sh
@@ -1,4 +1,23 @@
-docker run \
-              -p 8086:8086 \
-              -v myInfluxVolume:/var/lib/influxdb2 \
-              influxdb:latest
+#!/bin/bash
+
+# note : more params for setup, use github secrets
+# note : volume mount
+# fixme : password
+
+# simple start
+#docker run \
+#              -p 8086:8086 \
+#              -v myInfluxVolume:/var/lib/influxdb2 \
+#              influxdb:latest
+#
+
+# automated setup
+docker run -d -p 8086:8086 \
+      -v $PWD/influxdb/data:/var/lib/influxdb2 \
+      -v $PWD/influxdb/config.yml:/etc/influxdb2/config.yml \
+      -e DOCKER_INFLUXDB_INIT_MODE=setup \
+      -e DOCKER_INFLUXDB_INIT_USERNAME=my-user \
+      -e DOCKER_INFLUXDB_INIT_PASSWORD=my-password \
+      -e DOCKER_INFLUXDB_INIT_ORG=my-org \
+      -e DOCKER_INFLUXDB_INIT_BUCKET=my-bucket \
+      influxdb:2.0
