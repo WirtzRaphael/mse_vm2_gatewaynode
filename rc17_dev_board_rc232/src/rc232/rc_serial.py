7d576f9f4b3c2d00ecdf599110fc4491774ab69c
Raphael Wirtz <raphael9w@gmail.com>
2023-11-26 22:03:24 +0100

    chg: serial flow control

--- rc17_dev_board_rc232/src/rc232/rc_serial.py
+++ rc17_dev_board_rc232/src/rc232/rc_serial.py
@@ -3,14 +3,16 @@ import serial
 # eg linux '/dev/ttyUSB0'
 # eg windows 'COM3'
 
-def serial_init(serial_port, baud_rate):
+def serial_init(serial_port, baud_rate, timeout_s = 1):
     serial_object = serial.Serial(
         port=serial_port,
         baudrate=baud_rate,
+        #startbits=serial.STARTBITS_ONE,
         stopbits=serial.STOPBITS_ONE,
         parity=serial.PARITY_NONE,
         xonxoff=0,      # flow control
-        rtscts=0,       # rts/cts flow control
-        dsrdtr=0        # dsr/dtr flow control
+        rtscts=1,       # rts/cts flow control
+        dsrdtr=0,        # dsr/dtr flow control
+        timeout=timeout_s
     )
     return serial_object
