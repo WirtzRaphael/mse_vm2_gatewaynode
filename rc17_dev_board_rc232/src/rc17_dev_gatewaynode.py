a74161efe2678bb6de72d46087a02626582c68d1
Raphael Wirtz <raphael9w@gmail.com>
2023-12-03 20:24:46 +0100

    add: operation modes

--- rc17_dev_board_rc232/src/rc17_dev_gatewaynode.py
+++ rc17_dev_board_rc232/src/rc17_dev_gatewaynode.py
@@ -17,6 +17,12 @@ serial_port = '/dev/ttyUSB0'
 baud_rate = 19200 
 timeout = 1
 
+operation_mode = {
+    'production',
+    'debug'
+    'testing'
+}
+
 
 """ Functions
 """
@@ -40,29 +46,63 @@ def radio_read(serial_object: serial.Serial):
 
 """ Initialzation
 """
-dev_board_serial_20 = rc232.rc232_serial.SerialRcDevBoard(serial_port, baud_rate, timeout)
-dev_board_config_20 = rc232.rc232_config.RC232Configuration(channel=1, destination_id=20, power=0, rssi_mode=1, unique_id=10)
-serial_20 = rc232.rc232_serial.serial_init(dev_board_serial_20.port, dev_board_serial_20.baud_rate, dev_board_serial_20.timeout)
+def init_serial():
+    dev_board_serial_20 = rc232.rc232_serial.SerialRcDevBoard(serial_port, baud_rate, timeout)
+    dev_board_config_20 = rc232.rc232_config.RC232Configuration(channel=1, destination_id=20, power=0, rssi_mode=1, unique_id=10)
+    serial_20 = rc232.rc232_serial.serial_init(dev_board_serial_20.port, dev_board_serial_20.baud_rate, dev_board_serial_20.timeout)
+    try:
+        serial_20.open()
+    except serial.SerialException as e:
+        print(f"Serial communication error: {e}")
+    return serial_20
 
-try:
-    serial_20.open()
-except serial.SerialException as e:
-    print(f"Serial communication error: {e}")
+
+operation_mode = 'production'
+
+class ModeProduction:
+    def __init(self):
+        self.timer_repeated = None
+
+    def __enter__(self, *args, **kwargs):
+        print("RUN production mode \n")
+        # init
+        serial = init_serial()
+        # scheduling
+        self.timer_repeated = timeutil.timer.RepeatedTimer(1, radio_read, serial) # auto-starts
+        schedule.every().day.at("00:00").do(time_sync)
+
+    def __exit__(self, *args):
+        print("--EXIT production mode--")
+        self.timer_repeated.stop()
 
 
-""" Schedule
-"""
-schedule.every().day.at("00:00").do(time_sync)
-timer_repeated = timeutil.timer.RepeatedTimer(1, radio_read, serial_20) # auto-starts
+def run_mode_production():
+    return
+
+def run_mode_debug():
+    return
+
+def run_mode_testing():
+    measurments.rc_send.rc_send(serial_20, dev_board_config_20)
+    return
 
 while(True):
-    #radioConfigRead()
-    #print("running \n")
+    match operation_mode:
+        case 'production':
+            with ModeProduction():
+                while(operation_mode == 'production'):
+                    run_mode_production()
+        case 'debug':
+            print("debug \n")
+            run_mode_debug()
+        case 'testing':
+            print("testing \n")
+            run_mode_testing()
+        case _:
+            print("no mode\n")
     time.sleep(0.5)
-    
 
 
-#measurments.rc_send.rc_send(serial_10, dev_board_config_10)
 
 # Deinitialization
 try:
