db7d49c9356c229f6f7f0fe65d2797206bfee881
Raphael Wirtz <raphael9w@gmail.com>
2023-12-09 22:29:34 +0100

    fix: operation mode

--- rc17_dev_board_rc232/src/main.py
+++ rc17_dev_board_rc232/src/main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         match operation_mode:
             case 'gateway_pc':
                 with mode_gateway_pc.mode_gateway_pc():
-                    while(operation_mode == operation_mode{mode_gateway_pc}}):
+                    while(operation_mode == 'gateway_pc'):
                         mode_gateway_pc.run_mode_gateway_pc()
             case 'gateway_pi4':
                 print("MODE: raspberry pi \n")
