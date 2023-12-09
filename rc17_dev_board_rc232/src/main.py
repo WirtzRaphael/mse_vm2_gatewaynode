67c4257f53ed12dab28bdea163736a02aa179901
Raphael Wirtz <raphael9w@gmail.com>
2023-12-09 22:16:17 +0100

    fix:main

--- rc17_dev_board_rc232/src/main.py
+++ rc17_dev_board_rc232/src/main.py
@@ -1,10 +1,5 @@
 import mode_gateway_pc
-
-mode = {
-    'gateway_pc',
-    'gateway_pi4',
-    'performance_testing'
-}
+import mode_performance_testing
 
 def run_mode_gateway_pi4():
     return
@@ -16,14 +11,14 @@ if __name__ == "__main__":
         match operation_mode:
             case 'gateway_pc':
                 with mode_gateway_pc.mode_gateway_pc():
-                    while(operation_mode == 'gateway_pc'):
+                    while(operation_mode == operation_mode{mode_gateway_pc}}):
                         mode_gateway_pc.run_mode_gateway_pc()
             case 'gateway_pi4':
                 print("MODE: raspberry pi \n")
                 run_mode_gateway_pi4()
             case 'performance_testing':
                 print("MODE: performance testing \n")
-                run_mode_performance_testing()
+                mode_performance_testing.run_mode_performance_testing()
             case _:
                 print("no mode\n")
 
