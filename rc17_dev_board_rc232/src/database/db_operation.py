b46f3bc268fd41a1c309d91c55b6e54be4b1af93
Raphael Wirtz <raphael9w@gmail.com>
2023-12-14 13:49:20 +0100

    add: db with relative rtc time

--- rc17_dev_board_rc232/src/database/db_operation.py
+++ rc17_dev_board_rc232/src/database/db_operation.py
@@ -15,6 +15,7 @@ SQL_CREATE_GATEWAYNODE_TABLE = """ CREATE TABLE IF NOT EXISTS gatewaynode (
 SQL_CREATE_TEMPERATURE_1_TABLE = """ CREATE TABLE IF NOT EXISTS temperature_1 (
                                     id integer PRIMARY KEY,
                                     time_rtc integer NOT NULL,
+                                    time_relative_rtc integer NOT NULL,
                                     measure_id integer NOT NULL,
                                     temperature real NOT NULL
                                 ); """
@@ -22,26 +23,35 @@ SQL_CREATE_TEMPERATURE_1_TABLE = """ CREATE TABLE IF NOT EXISTS temperature_1 (
 SQL_CREATE_TEMPERATURE_2_TABLE = """ CREATE TABLE IF NOT EXISTS temperature_2 (
                                     id integer PRIMARY KEY,
                                     time_rtc integer NOT NULL,
+                                    time_relative_rtc integer NOT NULL,
                                     measure_id integer NOT NULL,
                                     temperature real NOT NULL
                                 ); """
 
-def insert_temperature_into_temperature1(connection, temperature):
-    sql = ''' INSERT INTO temperature_1(time_rtc, measure_id, temperature)
-              VALUES(?,?,?) '''
-    cursor = connection.cursor()
-    cursor.execute(sql, temperature)
+def insert_temperature_into_temperature1(connection, measurement_temperature):
+    try:
+        sql = ''' INSERT INTO temperature_1(time_rtc, time_relative_rtc, measure_id, temperature)
+                VALUES(?,?,?,?) '''
+        cursor = connection.cursor()
+        cursor.execute(sql, measurement_temperature)
+        
+        connection.commit()
+        
+        return cursor.lastrowid
     
-    connection.commit()
-    
-    return cursor.lastrowid
+    except Error as e:
+        return None
 
-def insert_temperature_into_temperature2(connection, temperature):
-    sql = ''' INSERT INTO temperature_2(time_rtc, measure_id, temperature)
-              VALUES(?,?,?) '''
-    cursor = connection.cursor()
-    cursor.execute(sql, temperature)
+def insert_temperature_into_temperature2(connection, measurement_temperature):
+    try:
+        sql = ''' INSERT INTO temperature_2(time_rtc, time_relative_rtc, measure_id, temperature)
+                VALUES(?,?,?,?) '''
+        cursor = connection.cursor()
+        cursor.execute(sql, measurement_temperature)
+        
+        connection.commit()
+        
+        return cursor.lastrowid
     
-    connection.commit()
-    
-    return cursor.lastrowid
+    except Error as e:
+        return None
