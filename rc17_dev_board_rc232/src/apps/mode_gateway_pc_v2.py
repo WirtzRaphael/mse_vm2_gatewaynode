82a22c3aa0266b3d4f9dba1a50013b7644762d84
Raphael Wirtz <raphael9w@gmail.com>
2024-06-29 23:46:27 +0200

    refactor: plot

--- rc17_dev_board_rc232/src/apps/mode_gateway_pc_v2.py
+++ rc17_dev_board_rc232/src/apps/mode_gateway_pc_v2.py
@@ -12,7 +12,6 @@ import re
 import pandas as pd
 from sqlalchemy import create_engine
 import plotnine as p9
-import matplotlib.pyplot as plt
 import yahdlc
 import serial
 import timeutil.timer
@@ -182,21 +181,16 @@ def insert_temperatures_testdata_into_database(db_connection:database.sqlite.DbC
 
 @repeat(every(2.5).seconds)
 def plot_measurements():
-    # todo : remove
-    with database.sqlite.DbConnection(DB_FILEPATH) as db_connection:
-        measurements_temperature = database.db_operation.read_temperature_from_measurements(connection = db_connection, node_id = 10, limit = 10)
-        print("Measurements temperature: ", measurements_temperature)
-
     # test data
-    measurements_temperature = {
-        'time_unix_s': [1609459200, 1609462800, 1609466400, 1609470000],
-        'sensor_value': [23.5, 24.0, 22.8, 23.1],
-        'node_id': [10, 10, 10, 10]
-    }
+    #measurements_temperature = {
+    #    'time_unix_s': [1609459200, 1609462800, 1609466400, 1609470000],
+    #    'sensor_value': [23.5, 24.0, 22.8, 23.1],
+    #    'node_id': [10, 10, 10, 10]
+    #}
+    #measurements_temperature_df = pd.DataFrame(measurements_temperature)
 
-    measurements_temperature_df = pd.DataFrame(measurements_temperature)
-
-    #measurements_temperature_df = database.db_operation.read_temperature_df_from_measurements(engine = sqlengine, node_id = 10, limit = 10)
+    # data from database
+    measurements_temperature_df = database.db_operation.read_temperature_df_from_measurements(engine = sqlengine, node_id = 10, limit = 10)
     print(measurements_temperature_df.head())
     # convert unix time to datetime
     measurements_temperature_df['time'] = pd.to_datetime(measurements_temperature_df['time_unix_s'], unit='s')
