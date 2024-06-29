677d4cc70c71e6b1fa10f59a0ddfa7a14ad615a8
Raphael Wirtz <raphael9w@gmail.com>
2024-06-29 20:54:39 +0200

    chg: db scheme and insert measurements

--- rc17_dev_board_rc232/src/database/db_operation.py
+++ rc17_dev_board_rc232/src/database/db_operation.py
@@ -6,8 +6,7 @@ SQL_CREATE_SENSORNODES_TABLE = """ CREATE TABLE IF NOT EXISTS sensornodes_info (
                                     time_unix_s integer NOT NULL,
                                     node_id integer NOT NULL,
                                     pico_id integer NOT NULL,
-                                    firmware_version integer NOT NULL,
-                                    radio_id integer
+                                    firmware_version integer NOT NULL
                                 ); """
                
 SQL_CREATE_SENSORNODES_SENSORS_TABLE = """ CREATE TABLE IF NOT EXISTS sensornodes_sensors (
@@ -16,20 +15,20 @@ SQL_CREATE_SENSORNODES_SENSORS_TABLE = """ CREATE TABLE IF NOT EXISTS sensornode
                                     sensor_id integer
                                 ); """
                                 
-SQL_CREATE_SENSORNODES_MEASUREMENTS_TABLE = """ CREATE TABLE IF NOT EXISTS sensornodes_sensor_data (
+SQL_CREATE_SENSORNODES_MEASUREMENTS_TABLE = """ CREATE TABLE IF NOT EXISTS sensornodes_measurements (
                                     id integer PRIMARY KEY,
+                                    node_id integer NOT NULL,
                                     time_unix_s integer NOT NULL,
                                     sensortype integer NOT NULL,
-                                    sensor_value real NOT NULL,
-                                    signal_strength integer
+                                    sensor_value real NOT NULL
                                 ); """
 
-def insert_temperature_into_sensor_data(connection, sensor_data):
+def insert_temperature_into_measurements(connection, measurements):
     try:
-        sql = ''' INSERT INTO sensornodes_sensor_data(time_receive_unix_s, time_rtc_s, measure_nr, sensornode_id, sensor_id, sensor_value, signal_strength)
-                VALUES(?,?,?,?,?,?,?) '''
+        sql = ''' INSERT INTO sensornodes_measurements(node_id, time_unix_s, sensortype, sensor_value
+        VALUES(?,?,?) '''
         cursor = connection.cursor()
-        cursor.execute(sql, sensor_data)
+        cursor.execute(sql, measurements)
         
         connection.commit()
         
