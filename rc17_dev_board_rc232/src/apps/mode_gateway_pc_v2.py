c08d4a5982ed66dfe94701a65b109a63300b1ca2
Raphael Wirtz <raphael9w@gmail.com>
2024-07-01 19:27:00 +0200

    chg: generic functions for temperature payload

--- rc17_dev_board_rc232/src/apps/mode_gateway_pc_v2.py
+++ rc17_dev_board_rc232/src/apps/mode_gateway_pc_v2.py
@@ -10,6 +10,7 @@ import schedule
 from schedule import every, repeat
 #import re
 import serial
+import datetime
 import timeutil.timer
 from datetime import datetime
 from sys import exit as sys_exit
@@ -89,7 +90,7 @@ def run_mode_gateway_pc_v2(operation_mode, rc_usb_port:serial, rc_usb_used:bool)
         #binary_data = radio.radio.read_received_data_from_file()
         try :
             # READ
-            print("Read data")
+            print("=> Read data")
             data_bytearray = radio.radio.radio_read(serial_rc)
 
             if data_bytearray is None:
@@ -99,21 +100,18 @@ def run_mode_gateway_pc_v2(operation_mode, rc_usb_port:serial, rc_usb_used:bool)
             radio.radio.radio_append_bytearray_to_file(data_bytearray)
 
             # FRAMES
-            print("Frames")
+            print("=> Frames")
             hdlc_frames = hdlc.hdlc.hdlc_decode_bytes(data_bytearray)
 
             for i, frame in enumerate(hdlc_frames):
                 print(f"Frame (bytearra) {i + 1}: {frame}")
                 print(f"Frame (hex) {i + 1}: {frame.hex()}")
 
-            # DECODE
-
             # fime: mulitple frames same time
             date_now = datetime.now()
             time_received_unix = datetime.timestamp(date_now)
-            time_measurement_s = time_received_unix
-            print("Decode")
             for i, frame in enumerate(hdlc_frames):
+                print("=> Frame")
                 frame_address = radio.radio.frame_get_hdlc_address(frame)
                 print(f"Frame address: {frame_address}")
                 frame_control = radio.radio.frame_get_hdlc_control(frame)
@@ -126,48 +124,36 @@ def run_mode_gateway_pc_v2(operation_mode, rc_usb_port:serial, rc_usb_used:bool)
                 payload = radio.radio.frame_get_data(frame)
                 print(f"Payload (hex): {payload.hex()}")
                 print(f"Payload (int): {int.from_bytes(payload, byteorder='little', signed=False)}")
+
+                # todo : wrong value (is 255)
+                #payload_node_address = radio.radio.frame_get_node_address(frame)
+                payload_node_address  = 10
+                print(f"Node address: {payload_node_address}")
                 #
-                # todo : remove last two bytes ?
                 match payload_content:
                     case 2:
                         # temperature 1
-                        print("temperature 1")
-                        temperatures = radio.radio.get_temperature_values_degree(payload)
-                        time_node_unix = radio.radio.get_temperature_time_unix(payload)
-                        print(f"Temperatures: {temperatures}")
-                        print(f"Time node unix: {time_node_unix}")
-                        print(datetime.fromtimestamp(time_node_unix).strftime('%Y-%m-%d %H:%M:%S'))
-                        # 
-                        print("Database")
-                        dbfilepath = r"gateway_v2.db"
-                        # todo : get from frame
-                        node_id = 10
-                        # todo : get from frame
-                        sensortype = 1
-                        # todo : time sync/diff with node
-
-                        for i, temperature in enumerate(temperatures):
-                            if temperature:
-                                time_measurement_s -= 5
-                                print(" ==> Insert into database")
-                                print(f"Measurement : {time_measurement_s}, {temperature}")
-                                insert_temperatures_into_database(dbfilepath,
-                                                      node_id,
-                                                      #time_received_unix_s,
-                                                      time_measurement_s,
-                                                      sensortype,
-                                                      temperature)
+                        process_payload_temperature(
+                            sensortype = 1,
+                            payload = payload,
+                            node_id=payload_node_address,
+                            time_measurement_s = time_received_unix
+                            )
                     case 3:
                         # temperature 2
-                        print("temperature 2")
+                        process_payload_temperature(
+                            sensortype = 2,
+                            payload = payload, 
+                            node_id=payload_node_address,
+                            time_measurement_s = time_received_unix
+                            )
                     case _:
                         print("Invalid frame content")
-
         except Exception as e:
             print(f"An error occurred: {e}")
 
         # PLOT
-        print("Plot")
+        print("=> Plot")
         visualize.plot_database.plot_measurements()
         time.sleep(1)
         
@@ -179,22 +165,52 @@ def run_mode_gateway_pc_v2(operation_mode, rc_usb_port:serial, rc_usb_used:bool)
     #radio_read(self.serial_rc)
     return
 
+def process_payload_temperature(
+        sensortype:int,
+        payload:bytearray,
+        node_id:int,
+        time_measurement_s:int):
+    print("==> Process temperature {sensortype}")
+    time_node_unix = radio.radio.get_temperature_time_unix(payload)
+    print(f"Time node unix: {time_node_unix}")
+    print(datetime.fromtimestamp(time_node_unix).strftime('%Y-%m-%d %H:%M:%S'))
+    #
+    temperatures = radio.radio.get_temperature_values_degree(payload)
+    print(f"Temperatures: {temperatures}")
+    #
+    print("--> Database")
+    # todo : time sync/diff with node
+    dbfilepath = r"gateway_v2.db"
+    insert_temperatures_into_database(
+        dbfilepath=dbfilepath,
+        node_id=node_id,
+        #time_unix=time_node_unix,
+        time_measurement_s=time_measurement_s,
+        sensortype=sensortype,
+        temperatures=temperatures
+        )
+    return None
 
-# todo : for v2
-def insert_temperatures_into_database(dbfilepath, node_id, time_unix, sensortype, temperature):
-    measurements = (
-        node_id,
-        time_unix,
-        sensortype,
-        temperature)
-    with database.sqlite.DbConnection(dbfilepath) as db_connection:
-        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurements = measurements)
+def insert_temperatures_into_database(dbfilepath, node_id, time_measurement_s, sensortype, temperatures):
+    for i, temperature in enumerate(temperatures):
+        if temperature:
+            time_measurement_s -= 5
+            print(" Insert into database")
+            print(f"Measurement : {time_measurement_s}, {temperature}")
+            measurement = (
+                node_id,
+                time_measurement_s,
+                sensortype,
+                temperature)
+            with database.sqlite.DbConnection(dbfilepath) as db_connection:
+                database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurement = measurement)
     return None
 
 def insert_temperatures_testdata_into_database(dbfilepath):
     with database.sqlite.DbConnection(dbfilepath) as db_connection:
-        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurements = (10, 1234567890, 1, 25.0))
-        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurements = (10, 1234567895, 1, 24.5))
-        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurements = (10, 1234567900, 1, 24.0))
-        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurements = (10, 1234567905, 1, 24.5))
+        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurement = (10, 1234567890, 1, 25.0))
+        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurement = (10, 1234567895, 1, 24.5))
+        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurement = (10, 1234567900, 1, 24.0))
+        database.db_operation.insert_temperature_into_measurements(connection = db_connection, measurement = (10, 1234567905, 1, 24.5))
     return None
+
